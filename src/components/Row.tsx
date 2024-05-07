ebd9122b29ea22ad64bc7c57d59b259716a47cb5
Thendup Tsering <thendupcodes@gmail.com>
2024-05-06 22:10:24 -0400

    Update styling logic for row cells

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,35 +1,60 @@
-export default function Row ({ guess, rIdx, mainWord, isSubmitted }: {
+const FULL_CORRECT_CLASS = 'Wordle__body-grid-cell--full-correct';
+const HALF_CORRECT_CLASS = 'Wordle__body-grid-cell--half-correct';
+const NOT_CORRECT_CLASS = 'Wordle__body-grid-cell--not-correct';
+const FILLED_CLASS = 'Wordle__body-grid-cell--filled';
+
+export default function Row ({ guess, rowIdx, answer, isSubmitted }: {
   guess: string,
-  rIdx: number,
-  mainWord: string,
+  rowIdx: number,
+  answer: string,
   isSubmitted: boolean,
 }) {
-  const chars: string[] = [];
+  const answerCompare = answer.split('');
 
-  for (let cIdx = 0; cIdx < 5; cIdx++) {
-    chars[cIdx] = guess?.[cIdx] || '';
+  const guessChars = Array(5).fill(null).map((_, idx) => {
+    const guessChar = guess?.[idx] || '';
+
+    return {
+      charKey: guessChar,
+      charClass: guessChar != '' ? FILLED_CLASS : '',
+    };
+  });
+
+  if (isSubmitted) {
+    // Instantiate all chars as being wrong, we will add correct classes later
+    guessChars.forEach((_, idx) => {
+      guessChars[idx].charClass = NOT_CORRECT_CLASS;
+    });
+
+    
+    // Style the charactes that are in the word AND in the correct spot
+    guessChars.forEach((guessChar, idx) => {
+      if (answer[idx] === guessChar.charKey) {
+        guessChars[idx].charClass = FULL_CORRECT_CLASS;
+        
+        // Modify the answer being compared for cases like duplicate letters in the guess
+        //   e.g. if answer=BREAK and guess=KAYAK, only 1 K and 1 A should be styled correctly
+        answerCompare.splice(answerCompare.indexOf(guessChar.charKey), 1);
+      }
+    });
+
+
+    // Style the charactes that are in the word BUT NOT in the correct spot
+    guessChars.forEach((guessChar, idx) => {
+      if (answerCompare.includes(guessChar.charKey) && guessChars[idx].charClass != FULL_CORRECT_CLASS) {
+        guessChars[idx].charClass = HALF_CORRECT_CLASS;
+        answerCompare.splice(answerCompare.indexOf(guessChar.charKey), 1);
+      }
+    });
   }
-
+ 
   return (
-    <div key={`row_${rIdx}`} className="Wordle__body-grid-row">
+    <div key={`row_${rowIdx}`} className="Wordle__body-grid-row">
       {
-        chars.map((char, idx) => {
-          let className = 'Wordle__body-grid-cell';
-          if (isSubmitted) {
-            if (mainWord[idx].toLowerCase() === char.toLowerCase()) {
-              className += ' Wordle__body-grid-cell--green';
-            } else if (mainWord.toLowerCase().includes(char.toLowerCase())) {
-              className += ' Wordle__body-grid-cell--orange';
-            }
-          }
-
-          if (char != '') {
-            className = className + ' Wordle__body-grid-cell--filled'
-          }
-
+        guessChars.map(({ charKey, charClass }, idx) => {
           return (
-            <div key={`row_col_${rIdx}_${idx}`} className={className}>
-              {char}
+            <div key={`row_col_${rowIdx}_${idx}`} className={`Wordle__body-grid-cell ${charClass}`}>
+              {charKey}
             </div>
           );
         })
