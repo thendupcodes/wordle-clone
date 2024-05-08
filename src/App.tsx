74008306c3503c8fd9b5d7c0ab41d7eb117e7e57
Thendup Tsering <thendupcodes@gmail.com>
2024-05-08 09:26:14 -0400

    Apply dark/light mode to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import ToastProvider from "@/components/ToastContext";
 import Wordle from "@/components/Wordle";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const toggle = () => {
+    setDarkMode(!darkMode);
+  }
+
   return (
     <ToastProvider>
-      <>
-        <div className="Wordle__header">
+      <div className={`App ${darkMode ? 'App--dark' : 'App--light'}`}>
+        <div className="Wordle__header" onClick={toggle}>
           <div className="Wordle__header-title">Wordle</div>
         </div>
 
         <Wordle />
-      </>
+      </div>
     </ToastProvider>
   )
 }
