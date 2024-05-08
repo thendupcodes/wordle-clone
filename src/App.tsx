985635abb88d46d783d42b6b316c4ec5276c9030
Thendup Tsering <thendupcodes@gmail.com>
2024-05-08 12:02:54 -0400

    Add toggle for high contrast mode

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,47 @@ import { useState } from "react";
 import Toggle from "./components/Toggle";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
+  const [highContrast, setHighContrast] = useState(false);
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
   }
 
+  const toggleContrast = () => {
+    setHighContrast(!highContrast);
+  }
+
   return (
-    <ToastProvider>
-      <div className={`App ${darkMode ? 'App--dark' : 'App--light'}`}>
-        <div className="Wordle__header">
-          <div className="Wordle__header-title">Wordle</div>
+    <div className={`App ${darkMode ? 'App--dark' : ''} ${highContrast ? 'App--high-contrast' : ''}`}>
+      <ToastProvider>
+        <>
+          <div className="Wordle__header">
+            <div className="Wordle__header-title">Wordle</div>
 
-          <div className="Wordle__header-buttons">
-            <Toggle
-              additionalClassNames="Toggle--dark-mode"
-              isToggled={darkMode}
-              handleToggle={toggleDarkMode}
-              offState={<i className="fa-solid fa-sun" />}
-              onState={<i className="fa-solid fa-moon" />}
-            />
+            <div className="Wordle__header-buttons">
+              <Toggle
+                additionalClassNames="Toggle--dark-mode"
+                isToggled={darkMode}
+                handleToggle={toggleDarkMode}
+                offState={<i className="fa-solid fa-sun" />}
+                onState={<i className="fa-solid fa-moon" />}
+              />
+
+              <Toggle
+                additionalClassNames="Toggle--contrast-mode"
+                isToggled={highContrast}
+                handleToggle={toggleContrast}
+                offState={<i className="fa-solid fa-circle-half-stroke" />}
+                onState={<i className="fa-solid fa-circle-half-stroke fa-flip-horizontal" />}
+              />
+            </div>
           </div>
-        </div>
 
-        <Wordle />
-      </div>
-    </ToastProvider>
+          <Wordle />
+        </>
+      </ToastProvider>
+    </div>
   )
 }
 
