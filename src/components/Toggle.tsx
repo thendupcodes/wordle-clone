c43e592c89f991bd0993a694301ed278857df6c3
Thendup Tsering <thendupcodes@gmail.com>
2024-05-08 11:30:26 -0400

    Add toggle component

---
+++ src/components/Toggle.tsx
@@ -0,0 +1,29 @@
+type ToggleProps = {
+  isToggled: boolean;
+  handleToggle: () => void;
+  offState: React.ReactNode; // Off-state icon component
+  onState: React.ReactNode; // On-state icon component
+  additionalClassNames?: string;
+};
+
+export default function Toggle ({
+  isToggled = false,
+  handleToggle,
+  offState,
+  onState,
+  additionalClassNames = '',
+} : ToggleProps) {
+  return (
+    <div className={`Toggle ${additionalClassNames} ${isToggled ? 'Toggle--toggled' : ''}`} onClick={handleToggle}>
+      <div className="Toggle__icon-wrapper">
+        <div className="Toggle__icon">
+          {isToggled ? (
+            onState
+          ) : (
+            offState
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
