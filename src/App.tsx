6c1988e80101e2a3df32d0be84025005ceadb7d4
Thendup Tsering <thendupcodes@gmail.com>
2024-05-08 11:30:44 -0400

    Implement dark mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import ToastProvider from "@/components/ToastContext";
 import Wordle from "@/components/Wordle";
 import { useState } from "react";
+import Toggle from "./components/Toggle";
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
 
-  const toggle = () => {
+  const toggleDarkMode = () => {
     setDarkMode(!darkMode);
   }
 
   return (
     <ToastProvider>
       <div className={`App ${darkMode ? 'App--dark' : 'App--light'}`}>
-        <div className="Wordle__header" onClick={toggle}>
+        <div className="Wordle__header">
           <div className="Wordle__header-title">Wordle</div>
+
+          <div className="Wordle__header-buttons">
+            <Toggle
+              additionalClassNames="Toggle--dark-mode"
+              isToggled={darkMode}
+              handleToggle={toggleDarkMode}
+              offState={<i className="fa-solid fa-sun" />}
+              onState={<i className="fa-solid fa-moon" />}
+            />
+          </div>
         </div>
 
         <Wordle />
