595cf7e481462819eb3352721266dac1f2328b0d
Thendup Tsering <thendupcodes@gmail.com>
2024-05-16 08:08:25 -0400

    Formatting ./src/components/Toggle.tsx

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,29 +1,28 @@
 type ToggleProps = {
-  isToggled: boolean;
-  handleToggle: () => void;
-  offState: React.ReactNode; // Off-state icon component
-  onState: React.ReactNode; // On-state icon component
-  additionalClassNames?: string;
+	isToggled: boolean;
+	handleToggle: () => void;
+	offState: React.ReactNode; // Off-state icon component
+	onState: React.ReactNode; // On-state icon component
+	additionalClassNames?: string;
 };
 
-export default function Toggle ({
-  isToggled = false,
-  handleToggle,
-  offState,
-  onState,
-  additionalClassNames = '',
-} : ToggleProps) {
-  return (
-    <div className={`Toggle ${additionalClassNames} ${isToggled ? 'Toggle--toggled' : ''}`} onClick={handleToggle}>
-      <div className="Toggle__icon-wrapper">
-        <div className="Toggle__icon">
-          {isToggled ? (
-            onState
-          ) : (
-            offState
-          )}
-        </div>
-      </div>
-    </div>
-  );
+export default function Toggle({
+	isToggled = false,
+	handleToggle,
+	offState,
+	onState,
+	additionalClassNames = '',
+}: ToggleProps) {
+	return (
+		<div
+			className={`Toggle ${additionalClassNames} ${
+				isToggled ? 'Toggle--toggled' : ''
+			}`}
+			onClick={handleToggle}
+		>
+			<div className="Toggle__icon-wrapper">
+				<div className="Toggle__icon">{isToggled ? onState : offState}</div>
+			</div>
+		</div>
+	);
 }
