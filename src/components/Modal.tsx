e6b6741d462daca9945f488f0d2b997dba4e577d
Thendup Tsering <thendupcodes@gmail.com>
2024-05-16 08:08:25 -0400

    Formatting ./src/components/Modal.tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,47 +1,49 @@
-import { ReactElement, useEffect } from "react";
-import { createPortal } from "react-dom";
+import { ReactElement, useEffect } from 'react';
+import { createPortal } from 'react-dom';
 
 type ModalProps = {
-  isOpen: boolean,
-  children: ReactElement,
-  onClose: () => void,
-  modalStyle: React.CSSProperties,
+	isOpen: boolean;
+	children: ReactElement;
+	onClose: () => void;
+	modalStyle: React.CSSProperties;
 };
 
-export default function Modal({ isOpen, children, onClose, modalStyle }: ModalProps) {
-  const handleKeyDown = (e: KeyboardEvent) => {
-    if (e.key === 'Escape') {
-      onClose();
-    }
-  }
+export default function Modal({
+	isOpen,
+	children,
+	onClose,
+	modalStyle,
+}: ModalProps) {
+	const handleKeyDown = (e: KeyboardEvent) => {
+		if (e.key === 'Escape') {
+			onClose();
+		}
+	};
 
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown, false);
-  
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown, false);
-    }
-  }, []);
+	useEffect(() => {
+		window.addEventListener('keydown', handleKeyDown, false);
 
-  if (!isOpen) return null;
-  
-  return createPortal((
-    <div className="Modal" style={modalStyle}>
-      <div className="Modal__overlay">
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown, false);
+		};
+	}, []);
 
-      </div>
-      
-      <div className="Modal__body">
-        <div className="Modal__body-close">
-          <button className="Modal__body-close-button" onClick={onClose}>
-            <i className="fa fa-light fa-xmark"></i>
-          </button>
-        </div>
+	if (!isOpen) return null;
 
-        <div className="Modal__body-children">
-          {children}
-        </div>
-      </div>
-    </div>
-  ), document.getElementById('portal'));
+	return createPortal(
+		<div className="Modal" style={modalStyle}>
+			<div className="Modal__overlay"></div>
+
+			<div className="Modal__body">
+				<div className="Modal__body-close">
+					<button className="Modal__body-close-button" onClick={onClose}>
+						<i className="fa fa-light fa-xmark"></i>
+					</button>
+				</div>
+
+				<div className="Modal__body-children">{children}</div>
+			</div>
+		</div>,
+		document.getElementById('portal')
+	);
 }
