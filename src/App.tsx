ba8ef4a82cacbe82949c3e3897f3a0ffc0b92d30
Thendup Tsering <thendupcodes@gmail.com>
2024-05-16 08:08:25 -0400

    Formatting ./src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,107 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import ToastProvider from "@/components/ToastContext";
-import Toggle from "@/components/Toggle";
-import Tooltip from "@/components/Tooltip";
-import Wordle from "@/components/Wordle";
+import ToastProvider from '@/components/ToastContext';
+import Toggle from '@/components/Toggle';
+import Tooltip from '@/components/Tooltip';
+import Wordle from '@/components/Wordle';
 
-import useLocalStorage from "./hooks/useLocalStorage";
+import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {
-  const appLocalStorage = useLocalStorage({ key: 'tt-wordle-app-state' })
+	const appLocalStorage = useLocalStorage({ key: 'tt-wordle-app-state' });
 
-  const [darkMode, setDarkMode] = useState(false);
-  const [highContrast, setHighContrast] = useState(false);
-  const [appTriggerModal, setAppTriggerModal] = useState(false);
+	const [darkMode, setDarkMode] = useState(false);
+	const [highContrast, setHighContrast] = useState(false);
+	const [appTriggerModal, setAppTriggerModal] = useState(false);
 
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-    appLocalStorage.setItem(JSON.stringify({
-      lsDarkMode: !darkMode,
-      lsHighContrast: highContrast,
-    }));
-  }
+	const toggleDarkMode = () => {
+		setDarkMode(!darkMode);
+		appLocalStorage.setItem(
+			JSON.stringify({
+				lsDarkMode: !darkMode,
+				lsHighContrast: highContrast,
+			})
+		);
+	};
 
-  const toggleContrast = () => {
-    setHighContrast(!highContrast);
-    appLocalStorage.setItem(JSON.stringify({
-      lsDarkMode: darkMode,
-      lsHighContrast: !highContrast,
-    }));
-  }
+	const toggleContrast = () => {
+		setHighContrast(!highContrast);
+		appLocalStorage.setItem(
+			JSON.stringify({
+				lsDarkMode: darkMode,
+				lsHighContrast: !highContrast,
+			})
+		);
+	};
 
-  useEffect(() => {
-    const storageDetails = appLocalStorage.getItem();
+	useEffect(() => {
+		const storageDetails = appLocalStorage.getItem();
 
-    if (storageDetails != null) {
-      const { lsDarkMode, lsHighContrast } = JSON.parse(storageDetails);
-      setDarkMode(lsDarkMode);
-      setHighContrast(lsHighContrast);
-    }
-  }, []);
+		if (storageDetails != null) {
+			const { lsDarkMode, lsHighContrast } = JSON.parse(storageDetails);
+			setDarkMode(lsDarkMode);
+			setHighContrast(lsHighContrast);
+		}
+	}, []);
 
-  return (
-    <div className={`App ${darkMode ? 'App--dark' : ''} ${highContrast ? 'App--high-contrast' : ''}`}>
-      <ToastProvider>
-        <>
-          <div className="Wordle__header">
-            <div className="Wordle__header-title">WordleClone</div>
+	return (
+		<div
+			className={`App ${darkMode ? 'App--dark' : ''} ${
+				highContrast ? 'App--high-contrast' : ''
+			}`}
+		>
+			<ToastProvider>
+				<>
+					<div className="Wordle__header">
+						<div className="Wordle__header-title">WordleClone</div>
 
-            <div className="Wordle__header-buttons">
-              <Tooltip direction="bottom" delay={0} content="See statistics">
-                <button onClick={() => setAppTriggerModal(true)} className="Wordle__header-stats-button">
-                  <i className="fa-solid fa-chart-column"></i>
-                </button>
-              </Tooltip>
+						<div className="Wordle__header-buttons">
+							<Tooltip direction="bottom" delay={0} content="See statistics">
+								<button
+									onClick={() => setAppTriggerModal(true)}
+									className="Wordle__header-stats-button"
+								>
+									<i className="fa-solid fa-chart-column"></i>
+								</button>
+							</Tooltip>
 
-              <Tooltip direction="bottom" delay={0} content="Toggle dark mode">
-                <Toggle
-                  additionalClassNames="Toggle--dark-mode"
-                  isToggled={darkMode}
-                  handleToggle={toggleDarkMode}
-                  offState={<i className="fa-solid fa-sun" />}
-                  onState={<i className="fa-solid fa-moon" />}
-                />
-              </Tooltip>
+							<Tooltip direction="bottom" delay={0} content="Toggle dark mode">
+								<Toggle
+									additionalClassNames="Toggle--dark-mode"
+									isToggled={darkMode}
+									handleToggle={toggleDarkMode}
+									offState={<i className="fa-solid fa-sun" />}
+									onState={<i className="fa-solid fa-moon" />}
+								/>
+							</Tooltip>
 
-              <Tooltip direction="bottom-left" delay={0} content="Toggle contrast">
-                <Toggle
-                  additionalClassNames="Toggle--high-contrast"
-                  isToggled={highContrast}
-                  handleToggle={toggleContrast}
-                  offState={<i className="fa-solid fa-circle-half-stroke" />}
-                  onState={<i className="fa-solid fa-circle-half-stroke fa-flip-horizontal" />}
-                />
-              </Tooltip>
-            </div>
-          </div>
+							<Tooltip
+								direction="bottom-left"
+								delay={0}
+								content="Toggle contrast"
+							>
+								<Toggle
+									additionalClassNames="Toggle--high-contrast"
+									isToggled={highContrast}
+									handleToggle={toggleContrast}
+									offState={<i className="fa-solid fa-circle-half-stroke" />}
+									onState={
+										<i className="fa-solid fa-circle-half-stroke fa-flip-horizontal" />
+									}
+								/>
+							</Tooltip>
+						</div>
+					</div>
 
-          <Wordle darkMode={darkMode} appTriggerModal={appTriggerModal} setAppTriggerModal={setAppTriggerModal} />
-        </>
-      </ToastProvider>
-    </div>
-  )
+					<Wordle
+						darkMode={darkMode}
+						appTriggerModal={appTriggerModal}
+						setAppTriggerModal={setAppTriggerModal}
+					/>
+				</>
+			</ToastProvider>
+		</div>
+	);
 }
 
-export default App
+export default App;
