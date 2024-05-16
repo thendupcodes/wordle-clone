a83ce60a926e41f572fbcd5cb69ab121333e5b8d
Thendup Tsering <thendupcodes@gmail.com>
2024-05-16 08:08:25 -0400

    Formatting ./src/components/Tooltip.tsx

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,37 +1,50 @@
 import { ReactElement, useState } from 'react';
 
 type TooltipProps = {
-  content: string,
-  children: ReactElement,
-  direction: 'top' | 'left' | 'bottom' | 'right' | 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right',
-  delay: number,
-}
+	content: string;
+	children: ReactElement;
+	direction:
+		| 'top'
+		| 'left'
+		| 'bottom'
+		| 'right'
+		| 'top-left'
+		| 'top-right'
+		| 'bottom-left'
+		| 'bottom-right';
+	delay: number;
+};
 
-export default function Tooltip ({
-  content, children, direction, delay = 200
+export default function Tooltip({
+	content,
+	children,
+	direction,
+	delay = 200,
 }: TooltipProps) {
-  const [show, setShow] = useState(false);
+	const [show, setShow] = useState(false);
 
-  const style = {
-    transition: `opacity ${delay}ms, visibility ${delay}ms`,
-    transitionDelay: `${delay}ms`,
-  };
+	const style = {
+		transition: `opacity ${delay}ms, visibility ${delay}ms`,
+		transitionDelay: `${delay}ms`,
+	};
 
-  return (
-     <div
-      tabIndex={0}
-      className="Tooltip-Container"
-      onMouseEnter={() => setShow(true)}
-      onMouseLeave={() => setShow(false)}
-    >
-      <div
-        style={style}
-        className={`Tooltip__tooltip ${!show ? 'Tooltip__tooltip--hide' : ''} Tooltip__tooltip--${direction}`}
-      >
-        {content}
-      </div>
-      
-      <div className="Tooltip__primary">{children}</div>
-    </div>
-  );
+	return (
+		<div
+			tabIndex={0}
+			className="Tooltip-Container"
+			onMouseEnter={() => setShow(true)}
+			onMouseLeave={() => setShow(false)}
+		>
+			<div
+				style={style}
+				className={`Tooltip__tooltip ${
+					!show ? 'Tooltip__tooltip--hide' : ''
+				} Tooltip__tooltip--${direction}`}
+			>
+				{content}
+			</div>
+
+			<div className="Tooltip__primary">{children}</div>
+		</div>
+	);
 }
