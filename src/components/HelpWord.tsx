731d04cbdc39ec42d7ff32ff4521165280a02e8c
Thendup Tsering <thendupcodes@gmail.com>
2024-05-27 11:59:47 -0400

    Add example state and index

--- src/components/HelpWord.tsx
+++ src/components/HelpWord.tsx
@@ -1,16 +1,18 @@
 type HelpWordProps = {
   word: string;
-  colors: string[];
+  state: string;
+  stateIdx: number;
+  highContrast: boolean;
 }
 
-export default function HelpWord({ word, colors }: HelpWordProps) {
+export default function HelpWord({ word, state, stateIdx, highContrast }: HelpWordProps) {
 	return (
-		<div className="Help-Word">
+		<div className={`Help-Word ${highContrast ? 'Help-Word--high-contrast ' : ''}`}>
       {word.split('').map((letter, idx) => {
         return (
           <div
             key={`${letter}-${idx}`}
-            className={`Help-Word__letter-container Help-Word__letter-container--${colors[idx]}`}
+            className={`Help-Word__letter-container ${stateIdx === idx ? `Help-Word__letter-container--${state}` : ''}`}
           >
             <div className="Help-Word__letter">{letter}</div>
           </div>
