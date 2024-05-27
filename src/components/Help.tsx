a984df9bc1b0b8502f16585aa612fc9d87b47f01
Thendup Tsering <thendupcodes@gmail.com>
2024-05-27 11:30:50 -0400

    Update help content

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -1,10 +1,14 @@
-export default function Help() {
+type HelpProps = {
+  highContrast: boolean
+;}
+
+export default function Help({ highContrast }: HelpProps) {
 	return (
 		<div className="Help">
       <div className="Help__heading">WordleClone</div>
 
       <div className="Help__main">
-        Welcome to Wordle! The goal of the game is to guess the hidden 5-letter word in six tries or less. Here’s how you can play:
+        Welcome to WordleClone! The goal of the game is to guess the hidden 5-letter word in six tries or less, BUT THERE IS A TWIST! In this game, all the words are terms or abbreviations you would hear in the software engineering world.
       </div>
 
       <div className="Help__body">
@@ -12,12 +16,13 @@ export default function Help() {
 
         <div className="Help__body-content">
           <ul>
-            <li>Type a 5-letter word and press the "Enter" key.</li>
-            <li>Make sure your guess is a valid word!</li>
-            <li>After you submit a guess, each letter will change color to give you hints about the hidden word:</li>
+            <li>Type a 5-letter word and press the "Enter" key to submit your guess.</li>
+
+            <li>After you submit a guess, each letter will change color to give you hints about the hidden word.</li>
+
             <ul>
-              <li><strong>Green</strong>: The letter is in the word and in the correct position.</li>
-              <li><strong>Yellow</strong>: The letter is in the word but in the wrong position.</li>
+              <li><strong>{highContrast ? 'Orange' : 'Green'}</strong>: The letter is in the word and in the correct position.</li>
+              <li><strong>{highContrast ? 'Blue' : 'Yellow'}</strong>: The letter is in the word but in the wrong position.</li>
               <li><strong>Gray</strong>: The letter is not in the word at all.</li>
             </ul>
           </ul>
@@ -30,7 +35,9 @@ export default function Help() {
         <div className="Help__body-content">
           <ul>
             <li>Start with common words that use a variety of letters.</li>
+
             <li>Pay attention to letter frequencies and common letter patterns.</li>
+
             <li>Use the color feedback to eliminate possibilities and zero in on the correct word.</li>
           </ul>
         </div>
